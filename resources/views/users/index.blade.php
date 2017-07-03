50c7b87a545b9de7c6f64be181f6dc2cbc5cc35f
SmoDav <smodavprivate@gmail.com>
2017-07-03 10:08:50 +0300

    adding users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,44 +1,44 @@
 @extends('layouts.app')
 
 @section('header')
-    <link href="{{ asset('assets/libs/jquery-datatables/css/dataTables.bootstrap.css') }}" rel="stylesheet"
-          type="text/css"/>
-    <link href="{{ asset('assets/libs/jquery-datatables/extensions/TableTools/css/dataTables.tableTools.css') }}"
-          rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/libs/jquery-datatables/css/dataTables.bootstrap.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/libs/jquery-datatables/extensions/TableTools/css/dataTables.tableTools.css') }}" rel="stylesheet" type="text/css"/>
 @endsection
 
 @section('content')
     @component('components.page-header')
         @slot('icon')
-            fa fa-anchor
+            fa fa-group
         @endslot
         @slot('header')
-            Stock
+            Users
         @endslot
-        Manage Stock
+        Manage System Users
     @endcomponent
 <div class="row">
     <div class="col-sm-12">
         <div class="widget">
             <div class="widget-header">
-                <h2><strong>Stock</strong></h2>
+                <h2><strong>Users</strong></h2>
             </div>
             <div class="widget-content padding">
                 <div class="table-responsive">
-                    <table class="table table-striped" id="stock-table" cellspacing="0">
+                    <table class="table table-striped" id="users-table" cellspacing="0">
                         <thead>
                         <tr>
-                            <th>Stall</th>
-                            <th>Item Id</th>
-                            <th>Quantity Received</th>
+                            <th>Full Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th></th>
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($stocks as $stock)
+                        @foreach($users as $user)
                             <tr>
-                                <td>{{ $stock->stall->name }}</td>
-                                <td>{{ $stock->item_id }}</td>
-                                <td>{{ $stock->quantity_reserved }}</td>
+                                <td>{{ $user->full_name }}</td>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td></td>
                             </tr>
                         @endforeach
                         </tbody>
@@ -54,6 +54,6 @@
     <script src="{{ asset('assets/libs/jquery-datatables/js/dataTables.bootstrap.js') }}"></script>
     <script src="{{ asset('assets/libs/jquery-datatables/extensions/TableTools/js/dataTables.tableTools.min.js') }}"></script>
     <script>
-        $('#stock-table').dataTable();
+        $('#users-table').dataTable();
     </script>
 @endsection
