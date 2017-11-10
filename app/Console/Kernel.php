6aab4ad856fda75b3e3d37b24349858d80070a22
Caroline <developmentkibugi@gmail.com>
2017-11-10 11:46:36 +0300

    making changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
 //        $schedule->command('demo:cron')
 //            ->everyMinute();
         $schedule->command('CronJob:cronjob')
-            ->dailyAt('9:20');
+            ->everyMinute();
     }
 
     /**
